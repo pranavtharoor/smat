766c368ab7966c4274ebdb041d461777e03168bb
pranavtharoor <pranav.tharoor@gmail.com>
2017-11-06 02:01:28 +0530

    Fixed bug in create function and added more documentation

--- smat.cpp
+++ smat.cpp
@@ -4,9 +4,10 @@
  */
 #include<iostream>
 using namespace std;
+
 class sparse
 { 
-    int row,col,value,a[50][50];
+    int row,col,value;
     public:
         void create(sparse*);
         void display(sparse*);
@@ -14,14 +15,19 @@ class sparse
         void spmult(sparse*,sparse*,sparse*);
         bool isSparse(sparse*);
 };
+
 //  Function to display a sparse matrix in triplet form(Row,column,value)
 void sparse::display(sparse *a)
 {  
+
+    // Checks if the matrix is empty
     if(a[0].value == 0)
     {
         cout<<"Matrix is empty\n";
         return ;
     }
+
+    // Prints the matrix in row, column, value format
     cout<<"\nR C V"<<endl;
     for(int i = 1;i <= a[0].value;i++)
     { 
@@ -33,24 +39,33 @@ void sparse::display(sparse *a)
 void sparse ::spmult(sparse *a,sparse *b,sparse *c)
 {  
     int m,k = 1,s;
+
+    // Checks if the matrices can be multiplied
     if(a[0].col != b[0].row)
     { 
         cout<<"Row,Column mismatch!Unable to multiply\n"<<endl;
         return ;
     }
+
+    // Iterates through matrices and performs multiplication
     for(int i = 1;i <= a[0].value;i++)
     { 
         for(int j = 1;j <= b[0].value;j++)
         {
             if(a[i].col == b[j].row)
             {
+
+                // Multiplies corresponding column and row elements and stores the result and index in a third sparse matrix 
                 c[k].value = a[i].value * b[j].value;
                 c[k].row = a[i].row;
                 c[k].col = b[j].col;
                 s = 0;
 
+                // Checks if product of elements in the same row of one matrix and column of the other already exists
                 if(c[k-1].row == c[k].row && c[k-1].col == c[k].col  && k-1 != 0)
                 {       
+
+                    // Adds the new product to the existing one and stores the sum
                     c[k-1].value = c[k-1].value+c[k].value;
                     k=k-1;
                     s=1;
@@ -58,9 +73,10 @@ void sparse ::spmult(sparse *a,sparse *b,sparse *c)
                 } 
                 k++; 
             }
-          } 
+        } 
     }
 
+    // Sets the number of rows, columns and elements in product matrix
     if(s == 0)
         c[0].value = k-1;
     else 
@@ -74,12 +90,14 @@ void sparse ::spmult(sparse *a,sparse *b,sparse *c)
 void sparse ::create(sparse *a)
 {  
     cout<<"Number of rows and columns in matrix:\n";
-    int m,n,l,k = 1,p,q;
+    int m,n,l,k = 1;
     cin>>m>>n;
     cout<<"Enter all elements in the appropriate order:\n";
-    for(int i = 0;i < n;i++)
+
+    // Accepts the matrix in regular format and stores it as a sparse matrix
+    for(int i = 0;i < m;i++)
     { 
-        for(int j = 0;j < m;j++)
+        for(int j = 0;j < n;j++)
         { 
             cin>>l;
             if(l!=0)
@@ -92,29 +110,11 @@ void sparse ::create(sparse *a)
 
         }    
    }
+
+   // Sets the number of rows, columns and elements in the matrix
    a[0].row = m;
    a[0].col = n;
    a[0].value = k-1;
-}
-
-//  Function to add two sparse matrices.
-void sparse::add(sparse a[],sparse b[])
-{ 
-    int m,i,n;
-    if(a[0].row>b[0].row)
-        m = a[0].row;
-    else 
-        m = b[0].row;
-    for(i = 0;i <= n;i++)
-    { 
-        a[i].value = a[i].value + b[i].value;
-    } i++;
-    while(i <= b[0].row)
-    {
-        a[i].value = b[i].value;
-        i++;
-    }
-
 }
 
 // Function to check if a matrix is sparse or not
@@ -134,6 +134,8 @@ int main()
     t.create(b);
     cout<<"Entered matrix in Row,Col,value triplet form:\n";
     t.display(b);
+
+    // Multiplies the matrices if they are sparse
     if(t.isSparse(a) && t.isSparse(b)) {
         t.spmult(a,b,c);
         cout<<"The product of the two matrices:\n ";
